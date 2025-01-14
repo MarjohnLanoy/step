c98cebd5b7b3d61091beb7ae0266bcaff685491c
MarjohnLanoy <marjohnlanoy@gmail.com>
2025-01-15 02:20:22 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import { use, useEffect, useState } from "react"
+import { useState } from "react"
 import "./index.css"
 
-const messages = ["Learn React", "Appy for jobs", "Invest your new income"]
+const messages = [
+  "Learn React",
+  "Appy for jobs",
+  "Invest your new income",
+  "Learn from Marjohn",
+]
 export default function App() {
   const [step, setStep] = useState(1)
   function nextClick() {
     if (step < messages.length) setStep(step + 1)
   }
   function prevClick() {
-    if (step >= messages.length - 1) setStep(step - 1)
+    if (step > 1) setStep(step - 1)
+    console.log(step, messages.length)
   }
 
   return (
@@ -26,7 +32,7 @@ function Numbers({ currentStep }) {
         return (
           <div
             key={step}
-            className={`${step + 1 === currentStep ? `active` : ``}`}
+            className={`${step + 1 <= currentStep ? `active` : ``}`}
           >
             {step + 1}
           </div>
@@ -39,7 +45,9 @@ function Numbers({ currentStep }) {
 function Message({ currentStep }) {
   return (
     <div className="message">
-      <p>{messages[currentStep - 1]}</p>
+      <p>
+        Step: {currentStep} {messages[currentStep - 1]}
+      </p>
     </div>
   )
 }
@@ -47,12 +55,16 @@ function Message({ currentStep }) {
 function Button({ addStep, minClick }) {
   return (
     <div className="buttons">
+      {/* Next Button */}
+
       <button
         onClick={minClick}
         style={{ backgroundColor: `#7920f2`, color: `#fff` }}
       >
         Previos
       </button>
+      {/* Previos Button */}
+
       <button
         onClick={addStep}
         style={{ backgroundColor: `#7920f2`, color: `#fff` }}
